074bbb100e5b02c40a923a4c57187ecac26fa4fd
shivu1919 <shivamthakur.ss129@gmail.com>
2024-10-21 14:28:22 +0530

    first

--- src/NumberIncreasingPyramid.java
+++ src/NumberIncreasingPyramid.java
@@ -8,7 +8,7 @@ public class NumberIncreasingPyramid {
 
         for(int i=1;i<=row;i++){
             for(int j=1;j<=i;j++){
-                System.out.print(i+" ");
+                System.out.print(j+" ");
             }
             System.out.println("");
         }
