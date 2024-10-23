9ed213db310e5ffabfbe6f9165f8a32014959af3
shivu1919 <shivamthakur.ss129@gmail.com>
2024-10-23 14:43:14 +0530

    abc

---
+++ src/diamond.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class diamond {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        System.out.println("Enter the number of rows");
+        int rows = s.nextInt();
+        for(int i=1;i<=rows;i++){
+            for(int j=1; j<=rows-i;j++){
+                System.out.print(" ");
+            }
+            for(int k=1;k<=i;k++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+
+        for(int i=rows-1;i>=1;i--){
+            for(int j=1; j<=rows-i;j++){
+                System.out.print(" ");
+            }
+            for(int k=1;k<=i;k++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+
+}
